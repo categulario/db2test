689c2920f5bf09b2feda6d7126f220eee4ee08ef
Abraham Toriz <awonderfulcode@gmail.com>
2018-05-07 09:26:28 -0500

    project initialized with maven

---
+++ src/main/java/com/categulario/db2test/App.java
@@ -0,0 +1,13 @@
+package com.categulario.db2test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
